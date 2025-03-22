885ebcdb5546bdcee7986e2ee6bd23910876d681
Mohammed Hussain <mohammedshussain@outlook.com>
2025-03-22 00:36:41 +0000

    fix: project underline dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -161,7 +161,7 @@ export default function Personal() {
                   target="_blank"
                 >
                   {project.name}
-                  <span className="absolute bottom-0.5 left-0 block h-[1px] w-full max-w-0 bg-zinc-900 transition-all duration-200 group-hover:max-w-full"></span>
+                  <span className="absolute bottom-0.5 left-0 block h-[1px] w-full max-w-0 bg-zinc-900 dark:bg-zinc-50 transition-all duration-200 group-hover:max-w-full"></span>
                 </a>
                 <p className="text-base text-zinc-600 dark:text-zinc-400">
                   {project.description}
