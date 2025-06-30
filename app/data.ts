dc8e251429dd61391aa42e3b6d4ee7fb4f053f7a
dan <dan.lettac@gmail.com>
2025-06-30 21:59:54 +0800

    edited the page of project eyes, added resume link

--- app/data.ts
+++ app/data.ts
@@ -56,7 +56,7 @@ export const PROJECTS: Project[] = [
     link: 'https://motion-primitives.com/',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
-    path: '',
+    path: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
     id: 'project2',
   },
 ]
@@ -130,6 +130,10 @@ export const SOCIAL_LINKS: SocialLink[] = [
   {
     label: 'LinkedIn',
     link: 'https://www.linkedin.com/in/dan-gabriel-lettac/',
+  },
+  {
+    label: 'Resume',
+    link: 'https://drive.google.com/file/d/1ZwGC5GXmYZVYWyxxTlnmzz7U20tKcmjx/view?usp=sharing',
   },
 ]
 
