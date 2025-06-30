1190d325d163510eaf307cd5498ca8b13f6c922d
dan <dan.lettac@gmail.com>
2025-06-30 23:11:31 +0800

    updated the portf project vid display

--- app/data.ts
+++ app/data.ts
@@ -55,8 +55,8 @@ export const PROJECTS: Project[] = [
       'Personalized forked from Nim — a free and open-source personal website template built with Next.js 15, React 19, Tailwind CSS v4, and Motion-Primitives',
     link: 'https://motion-primitives.com/',
     video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
-    path: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+      'https://res.cloudinary.com/ddzrtzzgt/video/upload/v1751295602/zzelif-nim-fork-portfolio_Deployment_Overview_Vercel_-_Opera_2025-06-30_22-52-55_kccksl.mp4',
+    path: '',
     id: 'project2',
   },
 ]
